485842f2ec4a8be32b50fbbf7307c9ab11d5899a
CattyForYou <dabielkkryzh@gmail.com>
2022-04-06 01:51:00 -0700

    done!

---
+++ encoder.cc
@@ -0,0 +1,40 @@
+//ENCODER!!!
+//Gabe H and Daniel K
+
+#include "huffman.hh"
+#include "bitio.hh"
+#include <cassert>
+#include <fstream>
+
+
+int main(int argc, char *argv[]) {
+	if(argc < 2){
+		std::cout << "ERROR: NO FILE TO COMPRESS, try again pls <3 \n";
+		return 0;
+	}
+	else {
+
+		std::ifstream is_file (argv[1], std::ifstream::in);
+
+		std::string filename = argv[1];
+
+		std::ofstream os_file (filename+".comp", std::ofstream::out);
+		BitOutput output(os_file);	
+		
+		Huffman encode_tree;
+		char symbol;
+
+		while(is_file.get(symbol)){
+			std::cout << symbol;
+			Huffman::bits_t char_bit = encode_tree.encode(symbol);
+			for(auto j : char_bit){
+				output.output_bit(j);
+			}
+		}
+		
+		is_file.close();
+		os_file.close();
+
+		return 0;
+	}
+}
